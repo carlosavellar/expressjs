04e60172d45186ec5dc534a9e33baa9c1605a679
José Carlos <carlos.avellar@gmail.com>
2018-10-17 15:50:03 -0300

    Initio

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+app.set('View Engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('currentYear', ()=>{
+    return new Date().getFullYear();
+});
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'A-bout page',
+        homeText: 'About everything you want.',
+    });
+});
+app.get('/',(req, res)=>{
+    res.render('index.hbs',{
+        pageTitle: 'Home Page',
+        homeText: 'This is the home content'
+   
+    });
+});
+
+app.get('/', (req, res)=>{
+      res.send({
+          name: 'José'
+      });
+});
+
+
+
+app.listen(port = 3000, (e) => {
+    console.log(`Server started on port ${port}`);
+});
