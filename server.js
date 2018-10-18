41509afc83eb0837c6e20c3dc8e744a7cf7bf3f4
José Carlos <carlos.avellar@gmail.com>
2018-10-18 10:16:06 -0300

    M aking a resuest form an Image

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 app.set('View Engine', 'hbs');
@@ -12,13 +13,24 @@ hbs.registerHelper('screamIt', (text)=>{
 });
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next)=>{
+    let now = new Date().toString();
+    let log = `${req.method} _  ${req.url} _ ${req.ips}`;
+    fs.appendFile('server.log', log + ' ' + now + '\n', (err)=>{
+        console.log('The "data to append" was appended to file!', err);
+    });
+    next();
+});
 
 app.get('/about', (req, res)=>{
     res.render('about.hbs',{
         pageTitle: 'About page',
         homeText: 'About everything you want.',
+        host: `${req.ips}`
     });
 });
+
+
 app.get('/',(req, res)=>{
     res.render('index.hbs',{
         pageTitle: 'Home Page',
